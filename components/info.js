0423d1f96eef765b922a174e79ad0e3ee59d3e24
botrufus <64660684+botrufus@users.noreply.github.com>
2021-05-01 18:52:27 +0200

    info 1 block del

--- components/info.js
+++ components/info.js
@@ -45,14 +45,6 @@ export default function bodyInfo() {
           sichern, bieten wir Ihnen folgende Dienstleistungen.
         </p>
       </div>
-      <div className="col-span-5 max-w-xl ml-36 border-2 p-4 border-black transition duration-100 ease-in-out transform hover:scale-110">
-        <h1 className="font-semibold text-3xl">Unsere Dienste</h1>
-        <p className="text-sm font-light">
-          Die Webseite, also die Präsentation Ihres Unternehmens im Internet,
-          ist heutzutage entscheidend über dessen Erfolg. Um diesen Erfolg zu
-          sichern, bieten wir Ihnen folgende Dienstleistungen.
-        </p>
-      </div>
     </div>
   );
 }
