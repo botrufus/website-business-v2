5635783b43027de989ecced2bff2139ada8e9a40
Ronchetto, Matthew <hello@doamatto.xyz>
2021-05-01 09:24:30 -0700

    chore: globalised Head and Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 
+import Head from '../components/header.js'
+import Footer from '../components/footer.js'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
