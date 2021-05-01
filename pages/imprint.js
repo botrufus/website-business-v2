8afb0244175bcbed4305a90ce96d6f4c24b5f697
Ronchetto, Matthew <hello@doamatto.xyz>
2021-05-01 09:16:30 -0700

    chore: added imprint page and link logic

---
+++ pages/imprint.js
@@ -0,0 +1,38 @@
+export default function Imprint() {
+    return (
+        <div>
+            <h1>Imprint</h1>
+            <br/>
+            <p>
+                Company Name
+                Registered Address
+                Munich, Germany
+
+                Phone: +49 000 000000-0
+                Email: placeholder [at] example.com (please note: support requests to this email address will be ignored.)
+            </p>
+            <p>
+                Represented by: Andreas Cselovszky
+                Responsible in accordance to Article 18 MStV: Andreas Cselovszky
+                VAT identification number: DE000000000
+            </p>
+            <p>
+                This information is provided according to the German laws (Section 5 Telemediengesetz (TMG) and Article 18 Medienstaatsvertrag (MStV)).
+            </p>
+
+            <h2>Disclaimer</h2>
+            <h3>Liability for contents</h3>
+            <p>
+                The contents of this site were created with great care; however, we cannot assume any liability for the correctness, completeness, and topicality of the contents. As a service provider, we are responsible for our own content on these pages according to Section 7, paragraph 1 TMG (German Telemedia Act) and general laws. According to sub-section 8 to 10 TMG, we are not obliged to monitor transmitted or stored external information or to investigate circumstances that indicate illegal activity. Obligations to remove or block the use of information according to general laws remain unaffected. If we become aware of any such infringements, we will remove such content immediately.
+            </p>
+            <h3>Liability for links</h3>
+            <p>
+                Our site may contains links to external websites controlled by third-parties, on whose contents we have no influence. We cannot assume liability for these external contents. The respective operator of the sites is responsible for the contents of the linked sites. The linked sites were checked for possible legal violations at the time of linking, however illegal contents were not recognisable. If we become aware of any infringements, we will remove such links immediately. 
+            </p>
+            <h3>Copyright</h3>
+            <p>
+                The contents and works on these pages created by the site operators are subject to German copyright law. The reproduction, editing, distribution and any kind of use outside the limits of copyright law requrie the written cosnent of the respective author or creator. Insofar as the content on the site was not created by the operator, the copyrights of third parties are respected. In particular, third-party content is marked as such. If we become aware of any infringements, we will remove such contents immediately. If you become aware of any infringements, please inform us acordingly.
+            </p>
+        </div>
+    )
+}
