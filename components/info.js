3f304f1e90af24534e1ab96ef999ebeff7cef745
botrufus <64660684+botrufus@users.noreply.github.com>
2021-05-01 19:11:26 +0200

    updated info text

--- components/info.js
+++ components/info.js
@@ -12,37 +12,33 @@ export default function bodyInfo() {
       </div>
       <div className="col-span-5"></div>
       <div className="col-span-5 max-w-xl ml-36 border-2 p-4 border-black transition duration-100 ease-in-out transform hover:scale-110">
-        <h1 className="font-semibold text-3xl">Unsere Dienste</h1>
+        <h1 className="font-semibold text-3xl">Sie Kontaktieren uns</h1>
         <p className="text-sm font-light">
-          Die Webseite, also die Präsentation Ihres Unternehmens im Internet,
-          ist heutzutage entscheidend über dessen Erfolg. Um diesen Erfolg zu
-          sichern, bieten wir Ihnen folgende Dienstleistungen.
+          Sie Kontaktieren uns über das Kontaktformular auf dieser Webseite. Wir erhalten Ihre Anfrage
+          und treten sobald wie möglich in Kontakt mit Ihnen.
         </p>
       </div>
       <div className="border-r-2 border-black row-span-5"></div>
       <div className="row-span-5"></div>
       <div className="col-span-5 max-w-xl ml-36 border-2 p-4 border-black transition duration-100 ease-in-out transform hover:scale-110">
-        <h1 className="font-semibold text-3xl">Unsere Dienste</h1>
+        <h1 className="font-semibold text-3xl">Wir besprechen alles nötige</h1>
         <p className="text-sm font-light">
-          Die Webseite, also die Präsentation Ihres Unternehmens im Internet,
-          ist heutzutage entscheidend über dessen Erfolg. Um diesen Erfolg zu
-          sichern, bieten wir Ihnen folgende Dienstleistungen.
+          Sie beschreiben uns Ihre Wünsche und wir beraten Sie. Falls Sie Schwierigkeiten haben oder keine Kreative Ader für 
+          Webseiten übrig haben, zeigen wir Ihnen Beispiele und machen Ihnen Vorschläge wie Ihre Webseite aussehen könnte.
         </p>
       </div>
       <div className="col-span-5 max-w-xl ml-36 border-2 p-4 border-black transition duration-100 ease-in-out transform hover:scale-110">
-        <h1 className="font-semibold text-3xl">Unsere Dienste</h1>
+        <h1 className="font-semibold text-3xl">Wir arbeiten an Ihrer Webseite</h1>
         <p className="text-sm font-light">
-          Die Webseite, also die Präsentation Ihres Unternehmens im Internet,
-          ist heutzutage entscheidend über dessen Erfolg. Um diesen Erfolg zu
-          sichern, bieten wir Ihnen folgende Dienstleistungen.
+          Sobald Ihre Wünsche geäußert sind, machen wir uns an die Arbeit um ihre Webseite so schnell wie möglich fertig zu stellen.
+          Währenddessen können sie alles live mitverfolgen und ihre Änderungenswünsche vorschlagen
         </p>
       </div>
       <div className="col-span-5 max-w-xl ml-36 border-2 p-4 border-black transition duration-100 ease-in-out transform hover:scale-110">
-        <h1 className="font-semibold text-3xl">Unsere Dienste</h1>
+        <h1 className="font-semibold text-3xl">Sie bekommen Ihre Webseite</h1>
         <p className="text-sm font-light">
-          Die Webseite, also die Präsentation Ihres Unternehmens im Internet,
-          ist heutzutage entscheidend über dessen Erfolg. Um diesen Erfolg zu
-          sichern, bieten wir Ihnen folgende Dienstleistungen.
+          Erst wenn Sie vollständig mit Ihrer neuen Webseite zufrieden sind, setzen wir alles nötige für Sie auf,
+          sodass ihrem digitalen Erfolg nichts mehr im Wege steht.
         </p>
       </div>
     </div>
